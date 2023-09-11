ad268ea2c2ce2c0b3ce5656002e00c84c2fef51a
mikietechie <mzinyoni7@outlook.com>
2023-09-11 23:47:46 +0200

    removed unwanted git files

--- apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SappChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sapp_chat'
-    icon = "fab fa-rocketchat"
